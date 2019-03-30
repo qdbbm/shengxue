7366510f2db3bdb4e4e4be84e9ed3755abdb3a45
qdbbm <582877596@qq.com>
2019-03-31 01:53:00 +0800

    首页、课程列表、课程详情

--- src/js/common.js
+++ src/js/common.js
@@ -7,6 +7,28 @@ $(function () {
         $(this).parent().siblings('.mod-tab-content').find('.mod-tab-pane:eq(' + $(this).index() + ')').removeClass('mbui-hide');
     })
 
+    // 搜索
+    $('.js-search').on('click', function () {
+        $(this).addClass('active');
+        $(this).find('input').focus();
+    })
+
+    $('.js-search').on('blur', 'input', function () {
+        $(this).parent().removeClass('active');
+    })
+
+    // 全文
+    $('.js-txt-more').on('click', function () {        
+        var _txtObj = $(this).siblings('.mumm-txt');
+        if (!_txtObj.hasClass('mumm-txt--more')) {
+            _txtObj.addClass('mumm-txt--more');
+            $(this).html('收起');
+        } else {
+            _txtObj.removeClass('mumm-txt--more');
+            $(this).html('全文');
+        }
+    })
+
 })
 
 function openShareTips() {
